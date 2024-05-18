f16bc0de595729940dc1288a58657dd81c1bcdc8
barry <kooksee@163.com>
2024-05-18 16:15:45 +0800

    fix: 2024-05-18 16:15:45

--- src/01-01.zig
+++ src/01-01.zig
@@ -4,6 +4,7 @@ const print = std.debug.print;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
